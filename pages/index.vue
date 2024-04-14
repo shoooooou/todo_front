2181afae9bf8ad7c39f5f2da1394f02c9b61a41e
s.nakamura <shotenba@gmail.com>
2024-04-14 20:43:28 +0900

    todolist繋ぎこみ付きのプロトタイプを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,56 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <nuxt-link to="/sandbox">aiueo</nuxt-link>
+    <h1>TodoApp</h1>
+    <h3>タスク名を入力して追加ボタンを入力してください！</h3>
+    <input v-model="inputTaskNameRef" type="text" /><button @click="addTask">
+      追加
+    </button>
+    <ul v-for="task in taskListRef" key="task">
+      <li>
+        {{ task.taskName }} <button @click="completeTask(task)">完了</button>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { getTaskList } from "../server/TaskService";
+import type { Task } from "../types/code";
+import { ref } from "vue";
+
+// タスク名
+const inputTaskNameRef = ref<string>("");
+// タスク一覧
+const taskListRef = ref<Task[]>([]);
+try {
+  // TODO: uidをユーザに合わせて設定したい
+  const taskList: Task[] = await getTaskList("0000000001");
+  taskListRef.value = taskList;
+} catch (e) {
+  console.log(e);
+}
+
+// タスク追加
+// TODO:APIを呼び出してDBにタスク一覧を保存したい
+const addTask = () => {
+  // 空文字の場合は追加しない
+  if (inputTaskNameRef.value === "") return;
+  // TODO: uidをユーザに合わせて設定したい
+  const addTask: Task = {
+    uid: "0000000001",
+    taskName: inputTaskNameRef.value,
+  };
+  taskListRef.value.push(addTask);
+  inputTaskNameRef.value = "";
+};
+
+// タスク完了
+// TODO:APIを呼び出してDBにタスク一覧を保存したい
+// TODO:同じタスク名が削除されてしまうのを解消したい
+const completeTask = (completeTask: Task) => {
+  taskListRef.value = taskListRef.value.filter(
+    (task) => task.taskName !== completeTask.taskName
+  );
+};
+</script>
