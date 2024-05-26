31d127388198f809d5161ce3d6cb10e24efbaf42
s.nakamura <shotenba@gmail.com>
2024-05-26 16:38:05 +0900

    エラーを改修

--- test/TaskService.test.ts
+++ test/TaskService.test.ts
@@ -1,4 +1,4 @@
-import * as Target from "../server/TaskService";
+import { taskService } from "../server/TaskService";
 import axios, { type AxiosResponse } from "axios";
 import type { Task } from "../types/code";
 
@@ -8,12 +8,12 @@ describe("getTaskList", () => {
   test("タスクリストが取得できる", async () => {
     const uid = "00000000000000000001";
     const expected: Task[] = [
-      { taskName: "todo1", statusCd: "1" },
-      { taskName: "todo2", statusCd: "2" },
-      { taskName: "todo3", statusCd: "3" },
+      { taskName: "todo1", statusCd: "0" },
+      { taskName: "todo2", statusCd: "1" },
+      { taskName: "todo3", statusCd: "2" },
     ];
     (axios.get as any).mockResolvedValue({ data: expected });
-    const taskList: Task[] = await Target.getTaskList(uid);
+    const taskList: Task[] = await taskService.getTaskList(uid);
     expect(taskList).toEqual(expected);
   });
 });
